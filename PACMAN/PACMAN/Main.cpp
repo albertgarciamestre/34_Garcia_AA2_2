4cd01eac929f262a99bbca9f5ef1b3c8945ee4c5
Albert <itus11108@gmail.com>
2019-03-17 19:24:19 +0100

    Cambios

---
+++ PACMAN/PACMAN/Main.cpp
@@ -0,0 +1,4 @@
+#include "Map.h"
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
--- PACMAN/PACMAN/Map.h
+++ PACMAN/PACMAN/Map.h
@@ -1,1 +1,6 @@
-#pragma once
+#include "Player.h"
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <iostream>
+#include <time.h>
