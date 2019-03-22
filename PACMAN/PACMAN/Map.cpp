b50fb5fb77d9a3ceb1a771e7dddf93e94dce7b83
Albert <itus11108@gmail.com>
2019-03-22 20:34:53 +0100

    PrintingBoard

--- PACMAN/PACMAN/Map.cpp
+++ PACMAN/PACMAN/Map.cpp
@@ -1,8 +1,11 @@
 #include "Map.h"
+char buffer[256];
 Map::Map() {
 	// Inicialitzem atributs
 	std::ifstream fileConfig("config.txt");
+
 	if (fileConfig.is_open()) {
+		
 		std::string firstLine;
 		fileConfig >> firstLine;
 		size_t prev = 0, pos = 0;
@@ -14,10 +17,22 @@ Map::Map() {
 
 		// Obtenim el nombre de linies de blocs
 		std::string secondLine;
-		fileConfig >> secondLine;
-		prev = 0, pos = 0;
-		pos = secondLine.find(" ", prev);
-
+		/*prev = 0, pos = 0;
+		pos = secondLine.find(";", prev);
+		buffer[256] = atoi((secondLine.substr(0, pos - prev)).c_str());*/
+		while (getline(fileConfig, secondLine)) 
+		{
+			std::cout <<secondLine << std::endl;
+		   
+		}
+		/*for (int i = 0; i < 256; i++) {
+			std::cout << buffer[i] << std::endl;
+			
+		}*/
+		fileConfig.close();
+		//pos = secondLine.find(" ", prev);
+		/*fileConfig.getline(buffer, 100);
+		std::cout << buffer << std::endl;*/
 	}
 	else
 	{
@@ -25,12 +40,41 @@ Map::Map() {
 		numColumns = NUM_COLUMNS;
 		
 	};
+	
 
-	InitializeMap();
+	//InitializeMap();
 }
 
 void Map::InitializeMap()
 {
+	int i;
+
+	map = new char*[numRows];
+	for (i = 0; i < numRows; i++) {
+		map[i] = new char[numColumns];
+	}
+
+
+	// Pinto blocs en les primeres lineBlocs linies
+	/*for (i = 0; i < numRows; i++) {
+		for (int j = 0; j < numColumns; j++) {
+			if (buffer[i] == 'X')
+			{
+				map[i][j] == 'X';
+			}
+			else if (buffer[i] == '*')
+			{
+				map[i][j] == '*';
+			}
+			else if (buffer[i] == ' ')
+			{
+				map[i][j] == ' ';
+			}
+			std::cout << map[i][j];
+				
+		}
+	}*/
+	
 	
 }
 
@@ -45,14 +89,14 @@ void Map::printMap() {
 
 	
 
-	for (int i = -1; i < numRows + 1; i++) {
-		for (int j = -1; j < numColumns + 1; j++) {
-					if (map[i][j] == 'X')
+	for (int i = 0; i < numRows ; i++) {
+		for (int j = 0; j < numColumns ; j++) {
+					/*if (map[i][j] == 'X')
 						SetConsoleTextAttribute(console, 12);	// Els blocs en vermell brillant
 						
-					else if (map[i][j] == '>')
+					else if (map[i][j] == '*')
 						SetConsoleTextAttribute(console, 10);	// La plataforma en verd brillant
-					std::cout << map[i][j];
+					std::cout << map[i][j];*/
 			
 		}
 		std::cout << std::endl;
