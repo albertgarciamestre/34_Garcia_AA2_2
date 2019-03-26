d5685cb5a5fe1a17804f829c1fcfa4893d793979
Albert <itus11108@gmail.com>
2019-03-26 01:09:11 +0100

    Lectura,matriz dinámica, pintado.
    Lectura del fichero txt. introducir el mapa dentro de una matriz dinámica y cambiar el color de los chars con el SetConsoleTextAttribute.

--- PACMAN/PACMAN/Map.cpp
+++ PACMAN/PACMAN/Map.cpp
@@ -1,11 +1,13 @@
 #include "Map.h"
-char buffer[256];
+#include <sstream>
+
+
 Map::Map() {
-	// Inicialitzem atributs
+	// initialize attributes
 	std::ifstream fileConfig("config.txt");
 
 	if (fileConfig.is_open()) {
-		
+
 		std::string firstLine;
 		fileConfig >> firstLine;
 		size_t prev = 0, pos = 0;
@@ -15,92 +17,60 @@ Map::Map() {
 		pos = firstLine.find(";", prev);
 		numRows = atoi((firstLine.substr(prev, pos - prev)).c_str());
 
-		// Obtenim el nombre de linies de blocs
+		//map
 		std::string secondLine;
-		/*prev = 0, pos = 0;
-		pos = secondLine.find(";", prev);
-		buffer[256] = atoi((secondLine.substr(0, pos - prev)).c_str());*/
-		while (getline(fileConfig, secondLine)) 
+		std::string tablero;
+		std::stringstream str;
+		str << fileConfig.rdbuf();
+		tablero = str.str();
+
+		map = new char*[numRows + 1];
+		for (int i = 0; i < numRows + 1; i++) {
+			map[i] = new char[numColumns + 1];
+		}
+
+		int x{ 0 };
+		int y{ 0 };
+
+		for (auto& c : tablero)
 		{
-			std::cout <<secondLine << std::endl;
-		   
+			map[x][y] = c;
+			y++;
+			if (y == numColumns) {
+				y = 0;
+				x++;
+			}
 		}
-		/*for (int i = 0; i < 256; i++) {
-			std::cout << buffer[i] << std::endl;
-			
-		}*/
 		fileConfig.close();
-		//pos = secondLine.find(" ", prev);
-		/*fileConfig.getline(buffer, 100);
-		std::cout << buffer << std::endl;*/
-	}
-	else
-	{
-		numRows = NUM_ROWS;
-		numColumns = NUM_COLUMNS;
-		
-	};
-	
 
-	//InitializeMap();
-}
-
-void Map::InitializeMap()
-{
-	int i;
-
-	map = new char*[numRows];
-	for (i = 0; i < numRows; i++) {
-		map[i] = new char[numColumns];
 	}
 
-
-	// Pinto blocs en les primeres lineBlocs linies
-	/*for (i = 0; i < numRows; i++) {
-		for (int j = 0; j < numColumns; j++) {
-			if (buffer[i] == 'X')
-			{
-				map[i][j] == 'X';
-			}
-			else if (buffer[i] == '*')
-			{
-				map[i][j] == '*';
-			}
-			else if (buffer[i] == ' ')
-			{
-				map[i][j] == ' ';
-			}
-			std::cout << map[i][j];
-				
-		}
-	}*/
-	
-	
 }
-
-
 
 
 void Map::printMap() {
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	// Reposicionem el cursor a l'inici de la consola per repintar sense esborrar (evitant el flickering o pampallugues)
+	// reposition the cursor at the start of the console to repaint without erasing (avoiding flickering or flashing)
 	SetConsoleCursorPosition(console, { 0,0 });
 
-	
+	SetConsoleTextAttribute(console, 11);	
+	std::cout << "Score--> " << score << "     " << std::endl;
 
-	for (int i = 0; i < numRows ; i++) {
+	for (int i = 0; i < numRows+1 ; i++) {
 		for (int j = 0; j < numColumns ; j++) {
-					/*if (map[i][j] == 'X')
-						SetConsoleTextAttribute(console, 12);	// Els blocs en vermell brillant
-						
+					if (map[i][j] == 'X')
+						SetConsoleTextAttribute(console, 16);
 					else if (map[i][j] == '*')
-						SetConsoleTextAttribute(console, 10);	// La plataforma en verd brillant
-					std::cout << map[i][j];*/
-			
+						SetConsoleTextAttribute(console, 15);
+					else if (map[i][j] == ' ')
+						SetConsoleTextAttribute(console, 1);
+					std::cout << map[i][j];
 		}
-		std::cout << std::endl;
+		
 	}
+	std::cout << std::endl;
+	
 }
 
 
